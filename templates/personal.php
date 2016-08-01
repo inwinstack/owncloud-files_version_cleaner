5b875b4c4d1b35bc438c47591c115d4321d7ba76
DaubaKao <dauba.k@inwinstack.com>
2016-08-01 17:06:22 +0800

    [update] add zh_TW translation

--- templates/personal.php
+++ templates/personal.php
@@ -5,7 +5,7 @@
 <div class="section" id="files_version_cleaner_section">
     <h2><?php p($l->t('Max number of files versions')); ?></h2>
     <div>
-        <label for="versionNumber"><?php p($l->t( 'Max number of files version' )); ?> </label>
+        <label for="versionNumber"><?php p($l->t( 'Max number of files versions' )); ?> </label>
         <select id="files_version_cleaner_personal_input">
             <?php for ($i = 1; $i <= $_["maxVersionNum"]; $i++) { ?>
                 <option value="<?php echo $i; ?>" <?php if($i == $_["userVersionNum"]) echo "selected"; ?>><?php echo $i; ?></option>
