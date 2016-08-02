7a7036cb4562bd5b592152cfbb6b95060b8dc6af
DaubaKao <dauba.k@inwinstack.com>
2016-08-02 16:30:49 +0800

    [update] check config key when installing app

---
+++ appinfo/install.php
@@ -0,0 +1,7 @@
+<?php
+if(!\OCP\Config::getSystemValue('files_version_cleaner_default_version_number')) {
+    \OCP\Config::setSystemValue('files_version_cleaner_default_version_number', 5);
+}
+if(!\OCP\Config::getSystemValue('files_version_cleaner_max_version_number')) {
+    \OCP\Config::setSystemValue('files_version_cleaner_max_version_number', 10);
+}
