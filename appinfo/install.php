e2a7ed3bc0edbbe87cc72c3038970b83e3d3d623
DaubaKao <dauba.k@inwinstack.com>
2016-08-09 17:51:21 +0800

    [feature] set config value when enable App

--- appinfo/install.php
+++ appinfo/install.php
@@ -5,3 +5,10 @@ if(!\OCP\Config::getSystemValue('files_version_cleaner_default_version_number'))
 if(!\OCP\Config::getSystemValue('files_version_cleaner_max_version_number')) {
     \OCP\Config::setSystemValue('files_version_cleaner_max_version_number', 10);
 }
+
+if(!\OCP\Config::getSystemValue('files_version_cleaner_default_historic_version_number')) {
+    \OCP\Config::setSystemValue('files_version_cleaner_default_historic_version_number', 5);
+}
+if(!\OCP\Config::getSystemValue('files_version_cleaner_max_historic_version_number')) {
+    \OCP\Config::setSystemValue('files_version_cleaner_max_historic_version_number', 10);
+}
