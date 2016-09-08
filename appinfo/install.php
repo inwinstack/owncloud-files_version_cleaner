13870ce7b7cba85dcdc027770f4e3ca4d4795b70
DaubaKao <dauba.k@inwinstack.com>
2016-09-08 11:57:59 +0800

    [update] create config key "files_version_cleaner_default_interval" when enagle app

--- appinfo/install.php
+++ appinfo/install.php
@@ -12,3 +12,7 @@ if(!\OCP\Config::getSystemValue('files_version_cleaner_default_historic_version_
 if(!\OCP\Config::getSystemValue('files_version_cleaner_max_historic_version_number')) {
     \OCP\Config::setSystemValue('files_version_cleaner_max_historic_version_number', 10);
 }
+
+if(!\OCP\Config::getSystemValue('files_version_cleaner_default_interval')) {
+    \OCP\Config::setSystemValue('files_version_cleaner_default_interval', 24);
+}
