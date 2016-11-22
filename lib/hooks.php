7e0bbc957664c2407b7f2fff0b2e75f03ede8e2e
DaubaKao <dauba.k@inwinstack.com>
2016-11-22 15:31:19 +0800

    [feature] add delete hook and rename hook

--- lib/hooks.php
+++ lib/hooks.php
@@ -2,6 +2,8 @@
 
 namespace OCA\Files_Version_Cleaner;
 
+use OCA\Files_Version_Cleaner\DatabaseVersionCleanerHandler;
+
 /**
  * Class Hooks
  * @author Dauba
@@ -44,9 +46,16 @@ class Hooks
     private $filesVersionCleaner;
 
     /**
+     * user root view
+     *
+     * @var \OC\Files\View
+     */
+    private $view;
+
+    /**
      * @param mixed $userFolder
      */
-    public function __construct($appName, $userSession, $userFolder, $config, $filesVersionCleaner)
+    public function __construct($appName, $userSession, $userFolder, $config, $filesVersionCleaner, $userRootView)
     {
         $this->uid = \OC_User::getUser();
         $this->userFolder = $userFolder;
@@ -54,6 +63,7 @@ class Hooks
         $this->userSession = $userSession;
         $this->config = $config;
         $this->appName = $appName;
+        $this->view = $userRootView;
     }
     
     /**
@@ -64,8 +74,61 @@ class Hooks
     public function register()
     {
         $postWriteCallback = array($this, "deleteVersion");
+        $preDeleteCallback = array($this, "preDeleteHook");
 
         $this->userFolder->listen("\OC\Files", "postWrite", $postWriteCallback);
+        $this->userFolder->listen("\OC\Files", "preDelete", $preDeleteCallback);
+
+        \OCP\Util::connectHook("OC_Filesystem", "post_rename", "\OCA\Files_Version_Cleaner\Hooks", "renameHook");
+    }
+
+    /**
+     * Hook for post rename, update file version cleaner data.
+     *
+     * @return void
+     */
+    public static function renameHook($params)
+    {
+        $view = new \OC\Files\View("/" . \OC_User::getUser() . "/files");
+        if($view->is_dir($params["newpath"])) {
+            DatabaseVersionCleanerHandler::updateData($params["oldpath"], $params["newpath"]);
+        }
+        else {
+            $oldDirName = dirname($params["oldpath"]);
+            $newDirName = dirname($params["newpath"]);
+            if (DatabaseVersionCleanerHandler::read($oldDirName)) {
+                if (!DatabaseVersionCleanerHandler::read($newDirName)) {
+                    \OCA\Files_Versions\Storage::markDeletedFile($params["oldpath"]);
+                    \OCA\Files_Versions\Storage::delete($params["oldpath"]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Hook for post delete, update file version cleaner data and delete versions of file or folder.
+     *
+     * @return void
+     */
+    public function preDeleteHook($node)
+    {
+        $view = new \OC\Files\View("/" . \OC_User::getUser() . "/files");
+        $relativePath = $view->getRelativePath($node->getPath());
+        if($node->getType() === "dir") {
+            DatabaseVersionCleanerHandler::deleteData($relativePath);
+            $this->filesVersionCleaner->cleanAllVersions($relativePath);
+        }
+        else {
+            $dirName = $view->getRelativePath($node->getParent()->getpath());
+            if (DatabaseVersionCleanerHandler::read($dirName)) {
+                $versions = \OCA\Files_Versions\Storage::getVersions($this->uid, $relativePath);
+                if (!empty($versions)) {
+                    foreach ($versions as $v) {
+                        $this->filesVersionCleaner->delete($v["path"], $v["version"]);
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -75,8 +138,7 @@ class Hooks
      */
     public function deleteVersion($fileNode)
     {
-        $view = new \OC\Files\View("/" . $this->uid . "/files");
-        $relativePath = $view->getRelativePath($fileNode->getFileInfo()->getPath());
+        $relativePath = $this->view->getRelativePath($fileNode->getFileInfo()->getPath());
         $versions[] = \OCA\Files_Versions\Storage::getVersions($this->uid, $relativePath);
         $this->filesVersionCleaner->deleteVersion($versions);
     }
