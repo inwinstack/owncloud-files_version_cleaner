369f231f2a657dc30e3d3493a023ee0f3958d109
Duncan Chiang <duncan.c@inwinstack.com>
2017-02-08 15:33:53 +0800

    [feature] Add new translate text.

--- l10n/zh_TW.js
+++ l10n/zh_TW.js
@@ -2,12 +2,17 @@ OC.L10N.register(
     "files_version_cleaner",
     {
     "Version control" : "版本控制",
-    "Are you sure to cancel version coltrol on this folder?" : "若取消版本控制，此資料夾底下所有檔案的版本都會消失，您確定要取消對此資料夾的版本控制？",
+    "Cancel version control" : "取消版本控制",
+    "Delete version" : "刪除版本",
+    "Revert version" : "還原版本",
+    "Are you sure to cancel version coltrol on this folder?" : "若取消版本控制，此資料夾底下所有檔案的版本皆會刪除，而且無法還原。",
     "Max number of files versions" : "保留區版本數量",
     "Max number of files historic versions" : "歷史區版本數量",
     "Change successful!" : "變更成功！",
     "Change unsuccessful!" : "變更失敗！",
     "Interval of per historic version (HR)" : "歷史版保存時間間隔 （小時）",
-    "Enable version control" : "啟用版本控制"
+    "Enable version control" : "啟用版本控制",
+    "Are you sure to delete this version ?" : "刪除版本後，您將無法透過本服務回收桶還原此版本。",
+    "Are you sure to revert this version ?": "這將會取代您檔案列表上的目前版本，而且無法無法還原。",
 }, 
 "nplurals=1; plural=0;");
